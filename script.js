d5d9af6da2a2607e4b1b622d8e2400b580fe0628
Vinicius Porto <viniciusporto010@gmail.com>
2024-06-09 19:20:12 -0300

    Adicionando a primeira versão do projeto de Geografia, projeto em etapa de finalização

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Adiciona evento de clique aos itens breadcrumb para rolar suavemente até a seção correspondente
+    document.querySelectorAll('.breadCrumb-item a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            const targetRect = targetElement.getBoundingClientRect();
+            const targetPosition = targetRect.top + window.scrollY;
+            const offsetPosition = targetPosition - (window.innerHeight / 2) + (targetElement.clientHeight / 2);
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
